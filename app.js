1aba5d36b9df56eca3c578e58312f569d6a1a9b5
Chris Balow Jr <cmbalow@illuminateed.net>
2019-06-22 10:49:25 -0400

    bug fixes for localStorage, but still needs work

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 // Initialize Modals
-$(document).ready(function(){
-  $('.modal').modal({
-  });
+$(document).ready(function () {
+  $('.modal').modal({});
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
   $('#new-task').modal({
-    onCloseEnd: function() {
+    onCloseEnd: function () {
       addTask();
     }
   });
 });
 
 // Define UI Variables
-const form          = document.querySelector('#task-form');
-const taskList      = document.querySelector('.collection');
-const newTask       = document.querySelector('.new-task');
-const clearBtn      = document.querySelector('.clear-tasks');
-const filter        = document.querySelector('#filter');
-const taskInput     = document.querySelector('#task');
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const newTask = document.querySelector('.new-task');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 const confirmDelete = document.querySelector('.confirm-delete');
-const confirmEdit   = document.querySelector('.confirm-edit');
-const confirmClear  = document.querySelector('.confirm-clear');
-const newTaskModal  = document.querySelector('#new-task');
+const confirmEdit = document.querySelector('.confirm-edit');
+const confirmClear = document.querySelector('.confirm-clear');
+const newTaskModal = document.querySelector('#new-task');
 
 // Load all event listeners
 loadEventListeners();
@@ -37,30 +36,29 @@ function loadEventListeners() {
 
 // Event Listener Functions
 function getTasks() {
-  let tasksArray = [];
-
+  tasksArray = [];
   if (localStorage.getItem('tasks') === null) {
     return;
   } else {
-    // let storedTasks = JSON.parse(localStorage.getItem('tasks'));
-    let storedTasks = localStorage.getItem('tasks');
-    tasksArray.push(storedTasks);
+    let storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    // tasksArray.push(storedTasks);
+    console.table(storedTasks);
+    storedTasks.forEach(task => {
+      const li = document.createElement('li');
+      li.className = 'collection-item';
+      li.style.background = '#333';
+      li.appendChild(document.createTextNode(task.title));
+      const deleteLink = document.createElement('a');
+      const editLink = document.createElement('a');
+      deleteLink.className = 'delete-item secondary-content modal-trigger';
+      deleteLink.innerHTML = '<i class="fa fa-remove"></i>';
+      editLink.className = 'edit-item secondary-content modal-trigger';
+      editLink.innerHTML = '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>';
+      li.appendChild(deleteLink);
+      li.appendChild(editLink);
+      taskList.appendChild(li);
+    });
   }
-
-  tasksArray.forEach(task => {
-    const li = document.createElement('li');
-    li.className = 'collection-item';
-    li.style.background = '#333';
-    li.appendChild(document.createTextNode(JSON.parse(task).title));
-    const deleteLink = document.createElement('a');
-    const editLink = document.createElement('a');
-    deleteLink.className = 'delete-item secondary-content modal-trigger';
-    deleteLink.innerHTML = '<class="fa fa-remove"></i>';
-    editLink.className = 'edit-item secondary-content modal-trigger';
-    editLink.innerHTML = '<i class="fa fa-pencil-square-i" aria-hidden"true"></i>';
-    li.appendChild(deleteLink);
-    taskList.appendChild(li);
-  });
 }
 
 function checkEmptyTask(e) {
@@ -74,10 +72,11 @@ function checkEmptyTask(e) {
 
 function openNewTaskModal(e) {
   if (e.target.classList.contains('new-task')) {
-    e.target.setAttribute('data-target', 'new-task')
+    e.target.setAttribute('data-target', 'new-task');
     // Insert task name as title in modal popup
-    newTaskModal.firstElementChild.firstElementChild.textContent = taskInput.value;
-  };
+    newTaskModal.firstElementChild.firstElementChild.textContent =
+      taskInput.value;
+  }
 }
 
 function addTask(e) {
@@ -94,25 +93,39 @@ function addTask(e) {
   li.appendChild(deleteLink);
   li.appendChild(editLink);
   taskList.appendChild(li);
-  
-  let newTaskDescription = document.querySelector('#new-task-description').value;
-  let newTaskDue         = document.querySelector('#new-task-due').value;
-  let newTaskPriority    = document.querySelector('#new-task-priority').value;
-  
-  storeTaskInLocalStorage(`{"title": "${taskInput.value}","description": "${newTaskDescription}","due": "${newTaskDue}","priority": "${newTaskPriority}"}`);
-  
-  taskInput.value = '';
 
+  let newTaskDescription = document.querySelector('#new-task-description')
+    .value;
+  let newTaskDue = document.querySelector('#new-task-due').value;
+  let newTaskPriority = document.querySelector('#new-task-priority').value;
+
+  let newTaskObj = {
+    title: `${taskInput.value}`,
+    description: `${newTaskDescription}`,
+    due: `${newTaskDue}`,
+    priority: `${newTaskPriority}`
+  };
+
+  storeTaskInLocalStorage(newTaskObj);
+
+  taskInput.value = '';
 }
 
 function storeTaskInLocalStorage(task) {
-
-  if (localStorage.getItem('tasks') === null) {
-    localStorage.setItem('tasks', task);
-  } else {
-    let newArray = [`${task}`];
-    newArray.push(localStorage.getItem('tasks'));
-    localStorage.setItem('tasks', JSON.stringify(newArray));
+  let tasksArray = [];
+  let storedTasks = JSON.parse(localStorage.getItem('tasks'));
+  if (storedTasks === null) {
+    tasksArray.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+  } else if (storedTasks.length === 1) {
+    // let storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    tasksArray.push(storedTasks);
+    tasksArray.push(task);
+    // storedTasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+  } else if (storedTasks.length > 1) {
+    storedTasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(storedTasks));
   }
 }
 
@@ -123,7 +136,7 @@ function editDeleteTask(e) {
     confirmDelete.addEventListener('click', () => {
       task.remove();
       removeTaskFromLocalStorage(task);
-    }); 
+    });
   } else if (e.target.parentElement.classList.contains('edit-item')) {
     e.target.parentElement.href = '#edit';
     let taskItem = e.target.parentElement.parentElement;
@@ -141,19 +154,16 @@ function editDeleteTask(e) {
 
 function removeTaskFromLocalStorage(taskItem) {
   let tasks;
-  
   if (localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-  
   tasks.forEach((task, index) => {
     if (taskItem.textContent === task) {
       tasks.splice(index, 1);
     }
   });
-
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
@@ -161,38 +171,35 @@ function replaceTask(oldTask, newTask) {
   if (newTask === '') {
     return;
   }
-
   let tasks;
-
   if (localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-
   tasks.forEach((task, index) => {
     if (oldTask === task) {
       tasks[index] = newTask;
     }
   });
-  
+
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function clearTasks(e) {
-    tasks = localStorage.getItem('tasks');
-    if (tasks === null) {
-      e.target.removeAttribute('data-target');
-    }
-    e.target.href = '#modal2';
-    confirmClear.addEventListener('click', () => {
-      localStorage.clear();
-    }); 
+  tasks = localStorage.getItem('tasks');
+  if (tasks === null) {
+    e.target.removeAttribute('data-target');
+  }
+  e.target.href = '#modal2';
+  confirmClear.addEventListener('click', () => {
+    localStorage.clear();
+  });
 }
 
 function filterTasks(e) {
   const text = e.target.value.toLowerCase();
-  document.querySelectorAll('.collection-item').forEach(function(task) {
+  document.querySelectorAll('.collection-item').forEach(function (task) {
     const item = task.firstChild.textContent;
     if (item.toLowerCase().indexOf(text) !== -1) {
       task.style.display = 'block';
